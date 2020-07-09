5886dff44d62648d73c5060ee95a788f31c7f463
brownspunbear <benjaminrshill@hotmail.com>
2020-07-09 12:32:55 +0100

    added text to main div

--- src/Component/Main/index.js
+++ src/Component/Main/index.js
@@ -9,6 +9,7 @@ class Main extends React.Component {
     render() {
         return (
             <div className="main">
+                <p>this is a test</p>
             </div>
         )
     }
