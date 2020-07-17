f93b03bf53b32877d183ae826566a65a24878a7a
unknown <Samuelcrudge@gmail.com>
2020-07-17 13:29:13 +0100

    Added new components and their Links to the navigation. Left comments to explain whats happening.

---
+++ src/Component/Contact/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './contact.css'
+
+
+
+class Contact extends React.Component {
+
+    render() {
+        return (
+            <div className="Contact">
+            </div>
+        )
+    }
+}
+
+export default Contact
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import {Route, Link, BrowserRouter as Router, Switch} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
-import Main from './Component/Main'
+// importing components so the Router div can pull the needed files.
+import Home from './Component/Home'
+import About from "./Component/About";
+import Portfolio from "./Component/Portfolio";
+import Contact from "./Component/Contact";
 import Lost from './Component/Lost'
 
 class Routing extends React.Component {
@@ -12,8 +16,21 @@ class Routing extends React.Component {
       return (
           <Router>
               <div>
+                  <ul className='Navigation'>
+                      {/*This creates the Nav component you will style in css.*/}
+                      <li>
+                          <Link to="/">Home</Link>
+                          <Link to="/about">ABOUT</Link>
+                          <Link to="/portfolio">PORTFOLIO</Link>
+                          <Link to="/contact">CONTACT</Link>
+                      </li>
+                  </ul>
                   <Switch>
-                            <Route exact path="/" component={ Main }/>
+                      {/*This will determine which components to use when the Links are press in the browser*/}
+                            <Route exact path="/" component={ Home }/>
+                            <Route exact path="/about" component={ About }/>
+                            <Route exact path="/portfolio" component={ Portfolio }/>
+                            <Route exact path="/contact" component={ Contact }/>
                             <Route component={ Lost }/>
                   </Switch>
               </div>
