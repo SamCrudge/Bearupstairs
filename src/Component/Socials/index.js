c25b53a24ce12ae76f05e3caa7e500841a8e2c06
brownspunbear <benjaminrshill@hotmail.com>
2020-07-18 22:19:33 +0100

    Tightening up z-index, removing unused icons

--- src/Component/Socials/index.js
+++ src/Component/Socials/index.js
@@ -19,13 +19,13 @@ class Socials extends React.Component {
                     <span>&#10010;</span>
                 </div>
                 <menu id="socialIcons">
-                    <a href="https://github.com/bearupstairs/">
-                        <img src={require("../../Assets/github.png")} className="iconOne" alt='Github logo' />
+                    <a href="https://instagram.com/bear.upstairs">
+                        <img src={require("../../Assets/instagram.png")} className="iconOne" alt='Github logo' />
                     </a>
-                    <a href="https://twitter.com/bearupstairs/">
+                    <a href="https://twitter.com/bearupstairs">
                         <img src={require("../../Assets/twitter.png")} className="iconTwo" alt='Twitter logo' />
                     </a>
-                    <a href="https://facebook.com/bearupstairs/">
+                    <a href="https://facebook.com/bearupstairs">
                         <img src={require("../../Assets/facebook.png")} className="iconThree" alt='Facebook logo' />
                     </a>
                 </menu>
