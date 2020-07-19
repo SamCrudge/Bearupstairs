287cad0d02577db8d376c059297567ab7290c36e
brownspunbear <benjaminrshill@hotmail.com>
2020-07-19 22:28:53 +0100

    Portfolio styling

--- src/Component/Portfolio/index.js
+++ src/Component/Portfolio/index.js
@@ -11,13 +11,16 @@ class Portfolio extends React.Component {
                 <h1>PORTFOLIO</h1>
                 <div className="portfolioBlock">
                     <div className="portfolioPage">
-                        <img src={require("../../Assets/portfolio_bsf_1.jpg")} alt='Desktop example' />
+                        <div className="title">&#10010; Artist gallery: bluesungfruit</div>
+                        <img src={require("../../Assets/portfolio/portfolio_bsf_1.jpg")} alt='Desktop example' />
                     </div>
                     <div className="portfolioPage">
-                        <img src={require("../../Assets/portfolio_vha_1.jpg")} alt='Desktop example' />
+                        <div className="title">&#10010; Victoria Hill Acupuncture</div>
+                        <img src={require("../../Assets/portfolio/portfolio_vha_1.jpg")} alt='Desktop example' />
                     </div>
                     <div className="portfolioPage">
-                        <img src={require("../../Assets/portfolio_ff_1.jpg")} alt='Desktop example' />
+                        <div className="title">&#10010; The Forester &amp; Flower Pub and B&amp;B</div>
+                        <img src={require("../../Assets/portfolio/portfolio_ff_1.jpg")} alt='Desktop example' />
                     </div>
                 </div>
             </div>
